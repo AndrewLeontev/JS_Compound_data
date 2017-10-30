255645ae32c8ad0029a77cfc27bea8615fdeaee7
Andrey Leontev <leontev.andrey.s@gmail.com>
2017-10-31 09:47:29 +1000

    Initial repo

---
+++ intro.js
@@ -0,0 +1,12 @@
+const x1 = 3;
+const y1 = 5;
+
+const x2 = -2;
+const y2 = 10;
+
+//точка по середине
+const x3 = middleX(x1, x2);
+const y3 = middleY(y1, y2);
+
+//Compound Data
+const middlePoint = middle(point1, point2);
